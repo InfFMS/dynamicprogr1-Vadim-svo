21e93b73c2ac3eec403a42c3ee58064fb304a0e3
Дягилев Вадим Георгиевич <v.dyagilev@main.nsu.ru>
2025-11-29 10:12:41 +0700

    tenzor vsyo eshyo delayetsya

--- task1.py
+++ task1.py
@@ -9,3 +9,31 @@
 """
 
 # Решение будет здесь
+
+import numpy as np
+
+sum = 0
+rang = 15
+
+
+a = [[[0 for _ in range(rang)] for _ in range (rang) ] for _ in range (rang)]
+
+a[0][0][0] = 20
+
+for x in range (rang):
+    for y in range(rang):
+        for z in range(rang):
+            if a[x][y][z] == 30:
+                sum += 1
+                continue
+
+            if a[x][y][z] > 0 and a[x][y][z] < 30:
+                if x+1 < rang:
+                    a[x+1][y][z] = a[x][y][z] + 1
+                if y+1 < rang:
+                    a[x][y+1][z] = a[x][y][z] + 2
+                if z+1 < rang:
+                    a[x][y][z+1] = a[x][y][z] + 4
+
+
+print(sum)
